e31cf19fef7b03a559c7740951fa3e0338319fef
Eric Hsu <erichsu7@gmail.com>
2015-05-18 15:20:20 -0700

    Finish 03-add-photos

--- js/tawkify.js
+++ js/tawkify.js
@@ -3,63 +3,26 @@
     window.Tawkify = {};
   }
 
-  var initializeSpinners = Tawkify.initializeSpinners = function () {
-    genderSpinner();
-    orientationSpinner();
-  }
+  var initializeDragAndDrop = Tawkify.initializeDragAndDrop = function () {
 
-  var genderSpinner = Tawkify.genderSpinner = function () {
-    var genders = ["Male", "Female"];
-    var genderIdx;
-
-    $(".input-gender .spinner-up").on("click", function (event) {
-      var $input = $(".input-gender > input");
-      if ($input.val() === "") {
-        genderIdx = 1;
-      } else {
-        genderIdx = (genderIdx + 1) % 2;
-      }
-
-      $input.val(genders[genderIdx]);
-    })
-
-    $(".input-gender .spinner-down").on("click", function (event) {
-      var $input = $(".input-gender > input");
-      if ($input.val() === "") {
-        genderIdx = 0;
-      } else {
-        genderIdx = (genderIdx + 1) % 2;
-      }
-
-      $input.val(genders[genderIdx]);
-    })
-  }
-
-  var orientationSpinner = Tawkify.orientationSpinner = function () {
-    var orientations = ["Men", "Women", "Both"];
-    var orIdx;
-    $(".input-orientation .spinner-up").on("click", function (event) {
-      var $input = $(".input-orientation > input");
-      if ($input.val() === "") {
-        orIdx = orientations.length - 1;
-      } else {
-        orIdx = orIdx === 0 ? orientations.length - 1 : orIdx - 1;
-      }
-
-      $input.val(orientations[orIdx]);
-
-    })
-
-    $(".input-orientation .spinner-down").on("click", function (event) {
-      var $input = $(".input-orientation > input");
-      if ($input.val() === "") {
-        orIdx = 0;
-      } else {
-        orIdx = (orIdx + 1) % orientations.length;
-      }
-
-      $input.val(orientations[orIdx]);
-    })
-  }
+    filepicker.makeDropPane($('.drag-and-drop-container')[0], {
+    multiple: true,
+    dragEnter: function() {
+      $(".drag-and-drop-instructions").html("Drop to upload");
+    },
+    dragLeave: function() {
+      $(".drag-and-drop-instructions").html("Drag and drop a photo");
+    },
+    onSuccess: function(Blobs) {
+      $(".drag-and-drop-instructions").text("Successfully uploaded photo! Drop another to upload.");
+    },
+    onError: function(type, message) {
+      $(".drag-and-drop-instructions").text('('+type+') '+ message);
+    },
+    onProgress: function(percentage) {
+      $(".drag-and-drop-instructions").text("Uploading ("+percentage+"%)");
+    }
+    });
+  };
 
 })();
